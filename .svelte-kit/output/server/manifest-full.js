b1f561c03bd189cb17cfd38ede023ba32f23d949
Jonathan Smith <jonathansmith@Jonathans-MacBook-Air.local>
2026-03-09 14:55:40 -0500

    Deploy with fixed base path

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.svg","icon-192.svg","icon-512.svg","manifest.json","robots.txt"]),
 	mimeTypes: {".svg":"image/svg+xml",".json":"application/json",".txt":"text/plain"},
 	_: {
-		client: {start:"_app/immutable/entry/start.B067JCvC.js",app:"_app/immutable/entry/app.Bz3TbdbO.js",imports:["_app/immutable/entry/start.B067JCvC.js","_app/immutable/chunks/DO1m7977.js","_app/immutable/chunks/qYgAWgle.js","_app/immutable/chunks/_tE6obZ3.js","_app/immutable/chunks/DOKlEXlX.js","_app/immutable/entry/app.Bz3TbdbO.js","_app/immutable/chunks/qYgAWgle.js","_app/immutable/chunks/Q4AlozVA.js","_app/immutable/chunks/C_NvWtst.js","_app/immutable/chunks/DOKlEXlX.js","_app/immutable/chunks/CixMHp53.js","_app/immutable/chunks/Cr9EBSKj.js","_app/immutable/chunks/_tE6obZ3.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.C-4L8vIv.js",app:"_app/immutable/entry/app.CJf-D-7h.js",imports:["_app/immutable/entry/start.C-4L8vIv.js","_app/immutable/chunks/VWhVtCb1.js","_app/immutable/chunks/qYgAWgle.js","_app/immutable/chunks/_tE6obZ3.js","_app/immutable/chunks/DOKlEXlX.js","_app/immutable/entry/app.CJf-D-7h.js","_app/immutable/chunks/qYgAWgle.js","_app/immutable/chunks/Q4AlozVA.js","_app/immutable/chunks/C_NvWtst.js","_app/immutable/chunks/DOKlEXlX.js","_app/immutable/chunks/CixMHp53.js","_app/immutable/chunks/Cr9EBSKj.js","_app/immutable/chunks/_tE6obZ3.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
