590dfd7434828fabb3534ce896d53731cce619e7
Jonathan Smith <jonathansmith@Jonathans-MacBook-Air.local>
2026-03-09 14:35:42 -0500

    Move build to root with relative paths

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Rh0Ybubn.js","_app/immutable/chunks/C_NvWtst.js","_app/immutable/chunks/qYgAWgle.js","_app/immutable/chunks/rEneL9nJ.js","_app/immutable/chunks/Q4AlozVA.js","_app/immutable/chunks/LvSQ2ViR.js","_app/immutable/chunks/ex5-JpY0.js","_app/immutable/chunks/_tE6obZ3.js","_app/immutable/chunks/DOKlEXlX.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.C_g3PoEz.js","_app/immutable/chunks/C_NvWtst.js","_app/immutable/chunks/qYgAWgle.js","_app/immutable/chunks/rEneL9nJ.js","_app/immutable/chunks/DOKlEXlX.js","_app/immutable/chunks/Q4AlozVA.js","_app/immutable/chunks/CixMHp53.js","_app/immutable/chunks/CuilDgjW.js","_app/immutable/chunks/LvSQ2ViR.js"];
-export const stylesheets = ["_app/immutable/assets/2.9TV1ucLe.css"];
-export const fonts = [];
